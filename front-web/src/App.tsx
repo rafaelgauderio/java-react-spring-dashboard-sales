231ef4d134138a0fdba154468e8faaed1740900c
Rafael de Luca <deluca1712@gmail.com>
2023-02-28 16:32:53 -0300

    Create filter component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import Header from './components/header';
+import Filter from './filter';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="app-container base-card">
+        <Filter />
+      </div>
     </>
   );
 }
