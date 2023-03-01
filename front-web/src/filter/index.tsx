c2b16b26b929f56ed5896eb35599e5f8543f54de
Rafael de Luca <deluca1712@gmail.com>
2023-03-01 11:39:42 -0300

    Date component using library flatpickr

--- front-web/src/filter/index.tsx
+++ front-web/src/filter/index.tsx
@@ -1,9 +1,36 @@
 import './styles.css';
+import FlatPicker from 'react-flatpickr';
+import 'flatpickr/dist/themes/material_green.css';
+import flatpickrLib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+
+// definindo o idioma do calendário como português
+flatpickrLib.localize(Portuguese);
 
 function Filter() {
+  // função que ao selecionar uma data do FlatPicker vai retornar uma lista de data pro backend
+  const onChangeDate = (listDates: Date[]) => {
+    console.log(listDates);
+  };
+
   return (
     <div className="filter-container base-card">
-      <h1>Filter Teste</h1>
+      <FlatPicker
+        options={{
+          dateFormat: 'd/m/Y',
+          mode: 'range',
+          showMonths: 3,
+        }}
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Selecione um período"
+      />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="MALE">Masculino</option>
+        <option value="OTHER">Outro</option>
+      </select>
     </div>
   );
 }
