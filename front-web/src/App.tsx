5c9818ff08fe2079ebb160acbb76adb5f2ac6fba
Rafael de Luca <deluca1712@gmail.com>
2023-03-02 16:27:27 -0300

    PieGraphicCard Component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,6 +3,7 @@ import Filter from './components/filter';
 import './App.css';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
+import PieGraphicCard from './components/pie-graphic-card/indext';
 
 function App() {
   return (
@@ -13,6 +14,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieGraphicCard
+            name="Lojas"
+            labels={['Foz do Iguaçu', 'Casvavel', 'Curitiba', 'Londrina']}
+            series={[25, 35, 25, 15]}
+          />
+          <PieGraphicCard
+            name="Pagamento"
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[50, 35, 15]}
+          />
         </div>
       </div>
     </>
---
+++ front-web/src/components/pie-graphic-card/helpers.ts
@@ -0,0 +1,68 @@
+import { ApexOptions } from 'apexcharts';
+
+export const buildPieGraphicConfig = (labels: string[] = [], name: string) => {
+  return {
+    labels,
+    noData: {
+      text: 'Sem resultados',
+      align: 'center',
+      verticalAlign: 'middle',
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+        color: '#FFF',
+        fontSize: '18px',
+        fontFamily: 'Roboto, sans-serif',
+      },
+    },
+    colors: ['#3e82f8', '#04d183', '#ffc106', '#ff6b71'],
+    legend: {
+      show: true,
+      floating: false,
+      position: 'bottom',
+      offsetY: 25,
+      labels: {
+        colors: ['#b4bed2'],
+      },
+      fontFamily: 'Roboto, sans-serif',
+      fontSize: '19x',
+      itemMargin: {
+        vertical: 5,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    plotOptions: {
+      pie: {
+        size: 400,
+        donut: {
+          size: '80%',
+          labels: {
+            show: true,
+            name: {
+              show: true,
+              offsetY: 10,
+              formatter: function () {
+                return name;
+              },
+            },
+            total: {
+              show: true,
+              showAlways: true,
+              fontSize: '25px',
+              color: '#ABB1C0',
+              fontFamily: 'Roboto, sans-serif',
+              formatter: function () {
+                return '';
+              },
+            },
+          },
+        },
+      },
+    },
+    chart: {
+      height: '400px',
+    },
+  } as ApexOptions;
+};
