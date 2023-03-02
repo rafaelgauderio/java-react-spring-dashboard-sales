e7d77861086bd99d1fb099e84b0917d7688a3d0c
Rafael de Luca <deluca1712@gmail.com>
2023-03-02 14:24:11 -0300

    SalesSummaryCard Component with static content

--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,5 +1,14 @@
+import './styles.css';
+import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
+
 function SalesSummaryCard() {
-  return <div className="sales-summary-card base-card"></div>;
+  return (
+    <div className="sales-summary-card base-card">
+      <DoneIcon />
+      <h2 className="sales-summary-card-value">399.50</h2>
+      <span className="sales-summary-card-label">Média</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;
